049fde4b04be3d5d5c237a1103413a74e06d68ec
Philipe Formagio Grosso <philipe.formagio@gmail.com>
2023-01-25 14:37:53 -0300

    GetAll added.

--- src/Expenses.Api/Controllers/ExpensesController.cs
+++ src/Expenses.Api/Controllers/ExpensesController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using System.Collections;
 
 namespace Expenses.Api.Controllers;
 
@@ -10,5 +11,15 @@ public class ExpensesController : ControllerBase
     public async Task<ActionResult<object>> Get()
     {
         return new { id = 1, expense = "Internet Bill" };
+    }
+
+    [HttpGet("expenses-all")]
+    public async Task<ActionResult<IEnumerable<object>>> GetAll()
+    {
+        return new List<object>
+        {
+            new { id = 1, expense = "Internet Bill" },
+            new { id = 2, expense = "Car" }
+        };
     }
 }
