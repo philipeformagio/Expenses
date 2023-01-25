f3c015361e45460533490a54c720af166dc913a3
Philipe Formagio Grosso <philipe.formagio@gmail.com>
2023-01-25 13:46:23 -0300

    ExpensesController created with dummy data.

--- src/Expenses.Api/Controllers/ExpensesController.cs
+++ src/Expenses.Api/Controllers/ExpensesController.cs
@@ -6,5 +6,9 @@ namespace Expenses.Api.Controllers;
 [Route("[controller]")]
 public class ExpensesController : ControllerBase
 {
-    
+    [HttpGet]
+    public async Task<ActionResult<object>> Get()
+    {
+        return new { id = 1, expense = "Internet Bill" };
+    }
 }
